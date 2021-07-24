1a04d0c0a4e6fadba13fa508d88ab00ad178ba9f
editorbank <>
2021-07-24 03:30:27 +0300

    построение проекта как flat или mono репозиторий

--- build.cmd
+++ build.cmd
@@ -1,2 +1,16 @@
 @echo Build ...
-call npm i && call npm run build && echo OK
+
+(
+  pushd sysGroup
+  call npm install
+  call npm run build
+  popd
+) && (
+  pushd sysObjects
+  call npm install
+  call npm run build
+  popd
+) && (
+  call npm install
+  call npm run build
+)
